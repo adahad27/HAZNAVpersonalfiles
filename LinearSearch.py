e10c263aa35b8ff6143ce90b868a9eeec896bf1e
Abhishek Dahad <abhishek.dahad@gmail.com>
2023-07-17 21:54:14 -0400

    some changes for optimization made, also tried adding visualization with matplotlib, both need more work

--- LinearSearch.py
+++ LinearSearch.py
@@ -17,13 +17,13 @@ def calc_residuals(beta, coordinates):
     return np.subtract(y_pred, coordinates[:,2])
 
 
-def polarConversion(xCoord, yCoord):
+def polarConversion(xCoord, yCoord, distance):
     #The parameters for this function are the predicted values from least squares
     #this function aims to return coordinates to travel to from the predicted values
     #it takes in the predicted values, decides what bearing to go in, and then returns the coordinates of the bearing after having travelled 'x' distance. In this case, 'hypotenuse' = x
     #as of yet, x is an arbitrary value of 3, this could be changed dynamically or set statically later after further testing
     courseAngle = math.atan(yCoord/xCoord)
-    hypotenuse = 4
+    hypotenuse = distance
     xCoordIncrement = hypotenuse * math.cos(courseAngle)
     yCoordIncrement = hypotenuse * math.sin(courseAngle)
     return [xCoordIncrement, yCoordIncrement]
@@ -31,7 +31,7 @@ def polarConversion(xCoord, yCoord):
 
 
 
-data = pd.read_csv(r"\\wsl.localhost\Ubuntu\home\adahad27\UMRHELab\HAZNAV\testfileout1.csv", index_col= "coordinates")
+data = pd.read_csv(r"HAZNAV/HAZNAVpersonalfiles/testfileout1.csv", index_col= "coordinates")
 #Had to put absolute path on my local computer here, this is only temporary for now.
 #r infront of a string means raw string, so no escape sequnces are used
 
@@ -48,8 +48,8 @@ drone = Drone()
 coordinates = np.array([[drone.xCoord, drone.yCoord, data.iloc[drone.xCoord, drone.yCoord]]])# the array should have 2 dimensions. 
 #1st column of this array contains x values, 2nd column of this array contains y values
 #we will continously have to append values to this to regress on
-coords = np.array([[1,0,data.iloc[1,0]], [0,1,data.iloc[0,1]], [1,1,data.iloc[1,1]]])
-coordinates = np.concatenate((coordinates, coords))
+# coords = np.array([[1,0,data.iloc[1,0]], [0,1,data.iloc[0,1]], [1,1,data.iloc[1,1]]])
+# coordinates = np.concatenate((coordinates, coords))
 #print(coordinates[:,0])# this returns the xCoords
 #print(coordinates[:,1])# this returns the yCoords
 # print(coordinates)
@@ -64,20 +64,22 @@ initial_guess = [1, 5, 5]
 # print(data.shape)
 
 
-while(data.iloc[drone.xCoord, drone.yCoord] != 2000): #this is hard coded right now, we will have to change this to a tolerance value because this is the main loop
+while(data.iloc[drone.xCoord, drone.yCoord] != 15): #this is hard coded right now, we will have to change this to a tolerance value because this is the main loop
     
     if(drone.xCoord + 1 >= 10 or drone.yCoord + 1 >= 10):
-        print("kys")
+        
         break
     
     currCoords = np.array([[drone.xCoord, drone.yCoord, data.iloc[drone.xCoord, drone.yCoord]]])
-    eastCoords = np.array([[drone.xCoord + 1, drone.yCoord, data.iloc[drone.xCoord + 1,drone.yCoord]]])
-    southCoords = np.array([[drone.xCoord, drone.yCoord + 1, data.iloc[drone.xCoord,drone.yCoord+1]]])
-    southeastCoords = np.array([[drone.xCoord + 1, drone.yCoord + 1, data.iloc[drone.xCoord,drone.yCoord+1]]])
+    extraIncrementArray = polarConversion(initial_guess[1], initial_guess[2],2)
+    extraCoords = np.array([[drone.xCoord + round(extraIncrementArray[0]), drone.yCoord + round(extraIncrementArray[1]), data.iloc[drone.xCoord + round(extraIncrementArray[0]), drone.yCoord + round(extraIncrementArray[1])]]])
+    # eastCoords = np.array([[drone.xCoord + 1, drone.yCoord, data.iloc[drone.xCoord + 1,drone.yCoord]]])
+    # southCoords = np.array([[drone.xCoord, drone.yCoord + 1, data.iloc[drone.xCoord,drone.yCoord+1]]])
+    # southeastCoords = np.array([[drone.xCoord + 1, drone.yCoord + 1, data.iloc[drone.xCoord,drone.yCoord+1]]])
     
     #this is creating an array of measurements to the east, south, and south east, so 4 measurements in total, I know this looks long, but I promise the thought behind it was not that complicated
 
-    coordinates = np.concatenate((coordinates, currCoords,eastCoords, southCoords, southeastCoords)) #the argument has to be passed in as a tuple idk why, probably some interface thing to make sure that nothing is modified?
+    coordinates = np.concatenate((coordinates, currCoords, extraCoords)) #the argument has to be passed in as a tuple idk why, probably some interface thing to make sure that nothing is modified?
 
     results = least_squares(calc_residuals, x0 = initial_guess, args=[coordinates])
 
@@ -85,7 +87,7 @@ while(data.iloc[drone.xCoord, drone.yCoord] != 2000): #this is hard coded right 
 
     initial_guess = results.x
 
-    incrementArray = polarConversion(results.x[1], results.x[2])
+    incrementArray = polarConversion(results.x[1], results.x[2],4)
     
     incrementArray[0] = round(incrementArray[0])
     incrementArray[1] = round(incrementArray[1])
@@ -97,8 +99,20 @@ while(data.iloc[drone.xCoord, drone.yCoord] != 2000): #this is hard coded right 
     
 
 
-
+print(coordinates)
 print(results.x)
+
+plotting_csv = data.to_numpy()
+
+fig, ax = plt.subplots()
+im = ax.imshow(plotting_csv)
+
+plt.colorbar(im)
+
+
+fig.tight_layout()
+
+plt.show()
 
 
 
