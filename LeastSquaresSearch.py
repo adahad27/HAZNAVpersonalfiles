7a1e7f1711a5cda790faeaa012b760d947332b13
Abhishek Dahad <abhishek.dahad@gmail.com>
2023-07-19 13:56:17 -0400

    A few bugfixes made, noise addition for measurements also added

--- LeastSquaresSearch.py
+++ LeastSquaresSearch.py
@@ -1,7 +1,7 @@
 import math
 import numpy as np
 from scipy.optimize import least_squares
-import pandas as pd
+#import pandas as pd
 from matplotlib import pyplot as plt
 import random
 
@@ -34,81 +34,72 @@ def LeastSquaresOneRun():
 
 
 
-
-    data = pd.read_csv(r"testfileout1.csv", index_col= "coordinates")
-    #Had to put absolute path on my local computer here, this is only temporary for now.
-    #r infront of a string means raw string, so no escape sequnces are used
-
-    #row2 = data.iloc[99,99]
-
-
     class Drone:
+        #This is just a struct for keeping this data glued together
         xCoord = 0
         yCoord = 0
 
     class radiationMap:
-        
-        def __init__(self, sourceX, sourceY, upperCoefficient):
+        #This map is meant to model the map of radiation, it returns radiation count based on position from the source
+        def __init__(self, sourceX, sourceY, upperCoefficient, noise):
             self.sourceX = sourceX
             self.sourceY = sourceY
-            
+            self.noise = noise
             self.upperCoefficient = upperCoefficient
             
 
         def getRadCount(self, xCoord, yCoord):
             if(xCoord == self.sourceX and yCoord == self.sourceY):
                 return 2000
+                #Setting the max of the value so that when the drone reaches here, it doesn't bug out
             else:
-                return self.upperCoefficient/((xCoord-self.sourceX)**2 + (yCoord-self.sourceY)**2)
+                noiseContribution = 0
+                if(self.noise):
+                    noiseContribution = random.random()/100
+                return self.upperCoefficient/((xCoord-self.sourceX)**2 + (yCoord-self.sourceY)**2) + noiseContribution
 
 
     drone = Drone()
-    radMap = radiationMap(sourceX = random.randint(0,10), sourceY = random.randint(0,10), upperCoefficient = random.randint(1,10))
+    #random.randint(0,10)
+    radMap = radiationMap(sourceX = 9 , sourceY = 7, upperCoefficient = 1, noise = False)
 
     coordinates = np.array([[drone.xCoord, drone.yCoord, radMap.getRadCount(drone.xCoord, drone.yCoord)]])# the array should have 2 dimensions. 
-    #1st column of this array contains x values, 2nd column of this array contains y values
-    #we will continously have to append values to this to regress on
-    # coords = np.array([[1,0,data.iloc[1,0]], [0,1,data.iloc[0,1]], [1,1,data.iloc[1,1]]])
-    # coordinates = np.concatenate((coordinates, coords))
-    #print(coordinates[:,0])# this returns the xCoords
-    #print(coordinates[:,1])# this returns the yCoords
-    # print(coordinates)
-    #okay so now we have data stored in an array that is n x 3
+    
     initial_guess = [1, 5, 5]
 
 
     currCoords = [drone.xCoord, drone.yCoord]
     previousCoords = [drone.xCoord + 1, drone.yCoord + 1]
-    # results = least_squares(calc_residuals, x0 = initial_guess, args = [coordinates]) #the reason why we have to store it in square brackets here is because there is an unpacking operator, but we don't want it to unpack the coordinates array, so we wrap it in another array, so that the coordinates array is sent as is
-    # print(results.x)
-    # print(data.shape)
-    prevResults = [0, 0, 0]
     
-    while(math.sqrt((currCoords[0] - previousCoords[0])**2 + (currCoords[1] - previousCoords[1])**2) > 0.000001 ): #this is hard coded right now, we will have to change this to a tolerance value because this is the main loop
-        
-        if(drone.xCoord + 1 >= 10 or drone.yCoord + 1 >= 10):
+    prevRes = [0, 0, 0]
+
+    travel_log = np.array([[0,0]]) #Replace (0,0) with the start position of the drone if it isn't (0,0)
+    result_log = np.array([initial_guess])
+
+    debugSum = 0
+    while(math.sqrt((initial_guess[1] - prevRes[0])**2 + (initial_guess[2] - prevRes[1])**2) > 0.000001 ): #this is hard coded right now, we will have to change this to a tolerance value because this is the main loop
+        debugSum += math.sqrt((initial_guess[1] - prevRes[0])**2 + (initial_guess[2] - prevRes[1])**2)
+        if(debugSum > 60):
+            #This is meant to prevent infinite loops from happening, the value of debugSum can be changed depending on testing data
+            break
+        if(drone.xCoord > 10 or drone.yCoord > 10 or drone.xCoord < -1 or drone.yCoord < -1):
             
             break
         
         
         currCoords = np.array([[drone.xCoord, drone.yCoord, radMap.getRadCount(drone.xCoord, drone.yCoord)]])
-        extraIncrementArray = polarConversion(initial_guess[1], initial_guess[2],2)
+        extraIncrementArray = polarConversion(initial_guess[1], initial_guess[2],0.5)
         extraCoords = np.array([[drone.xCoord + extraIncrementArray[0], drone.yCoord + extraIncrementArray[1], radMap.getRadCount(drone.xCoord + extraIncrementArray[0], drone.yCoord + extraIncrementArray[1])]])
-        # eastCoords = np.array([[drone.xCoord + 1, drone.yCoord, data.iloc[drone.xCoord + 1,drone.yCoord]]])
-        # southCoords = np.array([[drone.xCoord, drone.yCoord + 1, data.iloc[drone.xCoord,drone.yCoord+1]]])
-        # southeastCoords = np.array([[drone.xCoord + 1, drone.yCoord + 1, data.iloc[drone.xCoord,drone.yCoord+1]]])
         
-        #this is creating an array of measurements to the east, south, and south east, so 4 measurements in total, I know this looks long, but I promise the thought behind it was not that complicated
-
         coordinates = np.concatenate((coordinates, currCoords, extraCoords)) #the argument has to be passed in as a tuple idk why, probably some interface thing to make sure that nothing is modified?
 
         results = least_squares(calc_residuals, x0 = initial_guess, args=[coordinates])
 
         #I think we should change our initital guess according to the actual values that are outputted by the least squares method, we could also try using the Levenberg-Marquardt method, but that is a later problem
-
+        prevRes = initial_guess
         initial_guess = results.x
 
-        incrementArray = polarConversion(results.x[1], results.x[2],2)
+        incrementArray = polarConversion(results.x[1], results.x[2],0.5)
         
         
         
@@ -122,41 +113,33 @@ def LeastSquaresOneRun():
             courseAngle = math.atan((previousCoords[1]-currCoords[1])/(previousCoords[0]-currCoords[0])) + math.pi
             currCoords = previousCoords
             #CHANGE THE 2 HERE ACCORDING TO DISTANCE IN POLARCONVERSION, WAS TOO LAZY TO FIGURE OUT ANOTHER WAY OF DOING THIS
-            currCoords = [currCoords[0] + 2*math.cos(courseAngle), currCoords[1] + 2*math.sin(courseAngle)]
+            currCoords = [currCoords[0] + 0.5*math.cos(courseAngle), currCoords[1] + 0.5*math.sin(courseAngle)]
+        
+        
+        #This just updates the respective logs needed in case of testing/debugging
+        travel_log = np.concatenate((travel_log, [currCoords]))
+        result_log = np.concatenate((result_log, [results.x]))
+
         
 
-        
-
-
+    currCoords = [results.x[1], results.x[2]]
+    travel_log = np.concatenate((travel_log, [currCoords]))
     coordinateError =  math.sqrt((radMap.sourceX - results.x[1])**2 + (radMap.sourceY - results.x[2])**2)
     print("The actual source coordinates are (" + str(radMap.sourceX) +", " + str(radMap.sourceY) +") and the upper coefficient is " + str(radMap.upperCoefficient))
     print("The predicted source coordinates are (" + str(results.x[1]) + ", " + str(results.x[2]) +") and the upper coefficient is " + str(results.x[0]))
 
-
-
+    # print(travel_log)
+    # print(result_log)
     #Below is code responsible for plotting the path in a graph
-    xCoordList = coordinates[:, 0]
-    yCoordList = coordinates[:, 1]
+    xCoordList = travel_log[:, 0]
+    yCoordList = travel_log[:, 1]
 
-    plt.plot(xCoordList, yCoordList)
-    plt.scatter(radMap.sourceX, radMap.sourceY, c= "red")
-    plt.show()
+    # plt.plot(xCoordList, yCoordList)
+    # plt.scatter(radMap.sourceX, radMap.sourceY, c= "red")
+    # plt.show()
 
 
     return coordinateError
-    
-
-    # plotting_csv = data.to_numpy()
-
-    # fig, ax = plt.subplots()
-    # im = ax.imshow(plotting_csv)
-
-    # plt.colorbar(im)
-
-
-    # fig.tight_layout()
-
-    # plt.show()
 
 
 totalCoordinateError = 0
